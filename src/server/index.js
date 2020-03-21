62d6923e46aeb3ee77db319b9362d2918040b3c7
jamiekim828 <jamiekim828@gmail.com>
2020-03-21 18:52:48 +0100

    add service workers

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 var path = require('path');
+const webpack = require('webpack');
 const express = require('express');
 const mockAPIResponse = require('./mockAPI.js');
 var bodyParser = require('body-parser');
